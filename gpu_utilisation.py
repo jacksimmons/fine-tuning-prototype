8eb72047c3798432a0b68e5a94a2d90a5bc639a3
Jack Simmons <jack.simmons@mindsetpractice.com>
2025-09-02 14:01:27 +0100

    Added GPU utilisation print

---
+++ gpu_utilisation.py
@@ -0,0 +1,8 @@
+from pynvml import *
+
+
+def print_vram_usage():
+    nvmlInit()
+    handle = nvmlDeviceGetHandleByIndex(0)
+    info = nvmlDeviceGetMemoryInfo(handle)
+    print(f"GPU memory occupied: {info.used//1024**2} MB.")
