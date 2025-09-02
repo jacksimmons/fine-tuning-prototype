4e5853804e68b19232f2737825958ecf58656880
Jack Simmons <jack.simmons@mindsetpractice.com>
2025-09-02 13:13:56 +0100

    Moved evaluation to eval_model

--- fine_tuning.py
+++ fine_tuning.py
@@ -21,12 +21,9 @@ from peft import (
     get_peft_model,
     prepare_model_for_kbit_training
 )
-import evaluate
 import torch
-import time
-import numpy as np
-import pandas as pd
 from functools import partial
+from eval_model import qualitative, quantitative
 
 
 DASH_LINE = '-'.join('' for x in range(100))
@@ -259,84 +256,9 @@ ft_model = PeftModel.from_pretrained(
     is_trainable=False
 )
 
-# Evaluate model qualitatively (human evaluation)
-set_seed(SEED)
-index = 5
-dialogue = dataset['test'][index]['dialogue']
-summary = dataset['test'][index]['summary']
-prompt = f"Instruct: Summarize the following conversation.\n{dialogue}\nOutput:\n"
-
-peft_model_res = gen(ft_model,prompt,100,)
-peft_model_output = peft_model_res[0].split('Output:\n')[1]
-prefix, success, result = peft_model_output.partition('###')
-
-print(DASH_LINE)
-print(f'INPUT PROMPT:\n{prompt}')
-print(DASH_LINE)
-print(f'BASELINE HUMAN SUMMARY:\n{summary}\n')
-print(DASH_LINE)
-print(f'PEFT MODEL:\n{prefix}')
-
-# Evaluate model quantitatively (ROGUE metric)
-# Quantifies the validity of summarisations produced by models
-base_model = AutoModelForCausalLM.from_pretrained(
-    model_name, 
-    device_map='auto',
-    quantization_config=bnb_config,
-    trust_remote_code=True,
-)
-dialogues = dataset['test'][0:10]['dialogue']
-human_baseline_summaries = dataset['test'][0:10]['summary']
-
-original_model_summaries = []
-instruct_model_summaries = []
-peft_model_summaries = []
-
-for idx, dialogue in enumerate(dialogues):
-    human_baseline_text_output = human_baseline_summaries[idx]
-    prompt = f"Instruct: Summarize the following conversation.\n{dialogue}\nOutput:\n"
-    
-    original_model_res = gen(base_model,prompt,100,)
-    original_model_text_output = original_model_res[0].split('Output:\n')[1]
-    
-    peft_model_res = gen(ft_model,prompt,100,)
-    peft_model_output = peft_model_res[0].split('Output:\n')[1]
-    print(peft_model_output)
-    peft_model_text_output, success, result = peft_model_output.partition('###')
-
-    original_model_summaries.append(original_model_text_output)
-    peft_model_summaries.append(peft_model_text_output)
-
-zipped_summaries = list(zip(human_baseline_summaries, original_model_summaries, peft_model_summaries))
-
-df = pd.DataFrame(zipped_summaries, columns = ['human_baseline_summaries', 'original_model_summaries', 'peft_model_summaries'])
-rouge = evaluate.load('rouge')
-
-original_model_results = rouge.compute(
-    predictions=original_model_summaries,
-    references=human_baseline_summaries[0:len(original_model_summaries)],
-    use_aggregator=True,
-    use_stemmer=True,
-)
-
-peft_model_results = rouge.compute(
-    predictions=peft_model_summaries,
-    references=human_baseline_summaries[0:len(peft_model_summaries)],
-    use_aggregator=True,
-    use_stemmer=True,
-)
-
-print('ORIGINAL MODEL:')
-print(original_model_results)
-print('PEFT MODEL:')
-print(peft_model_results)
-
-print("Absolute percentage improvement of PEFT MODEL over ORIGINAL MODEL")
-
-improvement = (np.array(list(peft_model_results.values())) - np.array(list(original_model_results.values())))
-for key, value in zip(peft_model_results.keys(), improvement):
-    print(f'{key}: {value*100:.2f}%')
-
+# Evaluate the model
+qualitative(dataset, ft_model, SEED, gen)
+quantitative(dataset, model_name, bnb_config, ft_model, gen)
 
 ## YODA TUTORIAL REFERENCE
 # model_repo = "microsoft/Phi-3-mini-4k-instruct"
