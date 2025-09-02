8c63e6d67669b2bbc84d7653908b298f98b0f0e8
Jack Simmons <jack.simmons@mindsetpractice.com>
2025-09-02 13:05:46 +0100

    Initial commit

---
+++ fine_tuning.py
@@ -0,0 +1,453 @@
+from huggingface_hub import login
+import os
+from datasets import load_dataset
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    HfArgumentParser,
+    AutoTokenizer,
+    TrainingArguments,
+    Trainer,
+    DataCollatorForLanguageModeling,
+    GenerationConfig,
+    set_seed
+)
+from tqdm import tqdm
+from trl import SFTTrainer
+from peft import (
+    LoraConfig,
+    PeftModel,
+    get_peft_model,
+    prepare_model_for_kbit_training
+)
+import evaluate
+import torch
+import time
+import numpy as np
+import pandas as pd
+from functools import partial
+
+
+DASH_LINE = '-'.join('' for x in range(100))
+SEED = 42
+
+
+# Environment setup
+os.environ["WANDB_DISABLED"] = "true"
+
+# Login to HF
+login(os.environ.get("HF_TOKEN"))
+
+# Load dataset
+dataset = load_dataset("neil-code/dialogsum-test")
+
+# BNB setup
+compute_dtype = getattr(torch, "float16")
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=compute_dtype,
+    bnb_4bit_use_double_quant=False
+)
+
+# Get untrained model
+model_name = "microsoft/phi-2"
+model = AutoModelForCausalLM.from_pretrained(
+    pretrained_model_name_or_path=model_name,
+    device_map={"": 0},
+    quantization_config=bnb_config,
+    trust_remote_code=True
+)
+
+# Setup tokenizer
+tokenizer = AutoTokenizer.from_pretrained(
+    pretrained_model_name_or_path=model_name,
+    trust_remote_code=True,
+    padding_side="left",
+    add_eos_token=True,
+    add_bos_token=True,
+    use_fast=False
+)
+tokenizer.pad_token = tokenizer.eos_token
+
+# Test base model with zero-shot inferencing
+eval_tokenizer = AutoTokenizer.from_pretrained(
+    model_name,
+    trust_remote_code=True,
+    add_bos_token=True,
+    use_fast=False
+)
+eval_tokenizer.pad_token = eval_tokenizer.eos_token
+
+def gen(model, p, maxLen=100, sample=True):
+    toks = eval_tokenizer(p, return_tensors="pt")
+    res = model.generate(
+        **toks.to("cuda"),
+        max_new_tokens=maxLen,
+        do_sample=sample,
+        num_beams=1,
+        top_p=0.95
+    ).to("cpu")
+    return eval_tokenizer.batch_decode(res, skip_special_tokens=True)
+
+set_seed(SEED)
+index = 10
+prompt = dataset["test"][index]["dialogue"]
+summary = dataset["test"][index]["summary"]
+formatted_prompt = f"Instruct: Summarize the following conversation.\n{prompt}\nOutput:\n"
+res = gen(model, formatted_prompt, 100,)
+output = res[0].split("Output:\n")[1]
+
+print(DASH_LINE)
+print(f"INPUT PROMPT:\n{formatted_prompt}")
+print(DASH_LINE)
+print(f"BASELINE HUMAN SUMMARY:\n{summary}\n")
+print(DASH_LINE)
+print(f"MODEL GENERATION - ZERO SHOT:\n{output}")
+
+# Helper functions to format our input dataset for fine-tuning
+def create_prompt_formats(sample):
+    # Format fields of the sample ("instruction", "output"), concatenate them
+    # using two newlines.
+    INTRO_BLURB = "Below is an instruction describing a task. Write a response that appropriately completes the request."
+    INSTRUCTION_KEY = "### Instruct: Summarize the below conversation."
+    RESPONSE_KEY = "### Output:"
+    END_KEY = "### End"
+
+    blurb = f"\n{INTRO_BLURB}"
+    instruction = f"\n{INSTRUCTION_KEY}"
+    input_context = f"{sample["dialogue"]}" if sample["dialogue"] else None
+    response = f"{RESPONSE_KEY}\n{sample["summary"]}"
+    end = f"{END_KEY}"
+
+    parts = [blurb, instruction, input_context, response, end]
+    formatted_prompt = "\n\n".join(parts)
+    sample["text"] = formatted_prompt
+
+    return sample
+# SOURCE https://github.com/databrickslabs/dolly/blob/master/training/trainer.py
+def get_max_length(model):
+    conf = model.config
+    max_length = None
+    for length_setting in ["n_positions", "max_position_embeddings", "seq_length"]:
+        max_length = getattr(conf, length_setting, None)
+        if max_length:
+            print(f"Found max lenth: {max_length}")
+            break
+    if not max_length:
+        max_length = 1024
+        print(f"Using default max length: {max_length}")
+    return max_length
+def preprocess_batch(batch, tokenizer, max_length):
+    """
+    Tokenizing a batch
+    """
+    return tokenizer(
+        batch["text"],
+        max_length=max_length,
+        truncation=True,
+    )
+# SOURCE https://github.com/databrickslabs/dolly/blob/master/training/trainer.py
+def preprocess_dataset(tokenizer: AutoTokenizer, max_length: int, seed, dataset):
+    """
+    Format & tokenize it so it is ready for training
+    :param tokenizer (AutoTokenizer): Model Tokenizer
+    :param max_length (int): Maximum number of tokens to emit from tokenizer
+    """
+    
+    # Add prompt to each sample
+    print("Preprocessing dataset...")
+    dataset = dataset.map(create_prompt_formats)#, batched=True)
+    
+    # Apply preprocessing to each batch of the dataset & and remove 'instruction', 'context', 'response', 'category' fields
+    _preprocessing_function = partial(preprocess_batch, max_length=max_length, tokenizer=tokenizer)
+    dataset = dataset.map(
+        _preprocessing_function,
+        batched=True,
+        remove_columns=['id', 'topic', 'dialogue', 'summary'],
+    )
+
+    # Filter out samples that have input_ids exceeding max_length
+    dataset = dataset.filter(lambda sample: len(sample["input_ids"]) < max_length)
+    
+    # Shuffle dataset
+    dataset = dataset.shuffle(seed=seed)
+
+    return dataset
+
+# Pre-process dataset
+max_length = get_max_length(model)
+print(f"Model max length: {max_length}")
+train_dataset = preprocess_dataset(tokenizer, max_length, SEED, dataset["train"])
+eval_dataset = preprocess_dataset(tokenizer, max_length, SEED, dataset["validation"])
+
+# Prepare model for QLoRA
+model = prepare_model_for_kbit_training(model)
+
+# Setup PEFT for fine-tuning
+config = LoraConfig(
+    r=32, #Rank
+    lora_alpha=32,
+    target_modules=[
+        'q_proj',
+        'k_proj',
+        'v_proj',
+        'dense'
+    ],
+    bias="none",
+    lora_dropout=0.05,  # Conventional
+    task_type="CAUSAL_LM",
+)
+
+# Enable gradient checkpointing to reduce mem usage
+model.gradient_checkpointing_enable()
+peft_model = get_peft_model(model, config)
+peft_model.print_trainable_parameters()
+
+# Train PEFT adapter
+output_dir = f"./peft-dialogue-summary-training"
+peft_training_args = TrainingArguments(
+    output_dir = output_dir,
+    warmup_steps=1,
+    per_device_train_batch_size=1,
+    gradient_accumulation_steps=4,
+    max_steps=1000,
+    learning_rate=2e-4,
+    optim="paged_adamw_8bit",
+    logging_steps=25,
+    logging_dir="./logs",
+    save_strategy="steps",
+    save_steps=25,
+    eval_strategy="steps",
+    eval_steps=25,
+    do_eval=True,
+    gradient_checkpointing=True,
+    report_to="none",
+    overwrite_output_dir = 'True',
+    group_by_length=True,
+)
+peft_model.config.use_cache = False
+peft_trainer = Trainer(
+    model=peft_model,
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+    args=peft_training_args,
+    data_collator=DataCollatorForLanguageModeling(tokenizer, mlm=False),
+)
+peft_trainer.train()
+
+# Load the fine-tuned model from disk
+base_model = AutoModelForCausalLM.from_pretrained(
+    model_name, 
+    device_map='auto',
+    quantization_config=bnb_config,
+    trust_remote_code=True,
+    use_auth_token=True
+)
+eval_tokenizer = AutoTokenizer.from_pretrained(
+    model_name,
+    add_bos_token=True,
+    trust_remote_code=True,
+    use_fast=False
+)
+eval_tokenizer.pad_token = eval_tokenizer.eos_token
+ft_model = PeftModel.from_pretrained(
+    base_model,
+    "./peft-dialogue-summary-training/checkpoint-1000",
+    torch_dtype=torch.float16,
+    is_trainable=False
+)
+
+# Evaluate model qualitatively (human evaluation)
+set_seed(SEED)
+index = 5
+dialogue = dataset['test'][index]['dialogue']
+summary = dataset['test'][index]['summary']
+prompt = f"Instruct: Summarize the following conversation.\n{dialogue}\nOutput:\n"
+
+peft_model_res = gen(ft_model,prompt,100,)
+peft_model_output = peft_model_res[0].split('Output:\n')[1]
+prefix, success, result = peft_model_output.partition('###')
+
+print(DASH_LINE)
+print(f'INPUT PROMPT:\n{prompt}')
+print(DASH_LINE)
+print(f'BASELINE HUMAN SUMMARY:\n{summary}\n')
+print(DASH_LINE)
+print(f'PEFT MODEL:\n{prefix}')
+
+# Evaluate model quantitatively (ROGUE metric)
+# Quantifies the validity of summarisations produced by models
+base_model = AutoModelForCausalLM.from_pretrained(
+    model_name, 
+    device_map='auto',
+    quantization_config=bnb_config,
+    trust_remote_code=True,
+)
+dialogues = dataset['test'][0:10]['dialogue']
+human_baseline_summaries = dataset['test'][0:10]['summary']
+
+original_model_summaries = []
+instruct_model_summaries = []
+peft_model_summaries = []
+
+for idx, dialogue in enumerate(dialogues):
+    human_baseline_text_output = human_baseline_summaries[idx]
+    prompt = f"Instruct: Summarize the following conversation.\n{dialogue}\nOutput:\n"
+    
+    original_model_res = gen(base_model,prompt,100,)
+    original_model_text_output = original_model_res[0].split('Output:\n')[1]
+    
+    peft_model_res = gen(ft_model,prompt,100,)
+    peft_model_output = peft_model_res[0].split('Output:\n')[1]
+    print(peft_model_output)
+    peft_model_text_output, success, result = peft_model_output.partition('###')
+
+    original_model_summaries.append(original_model_text_output)
+    peft_model_summaries.append(peft_model_text_output)
+
+zipped_summaries = list(zip(human_baseline_summaries, original_model_summaries, peft_model_summaries))
+
+df = pd.DataFrame(zipped_summaries, columns = ['human_baseline_summaries', 'original_model_summaries', 'peft_model_summaries'])
+rouge = evaluate.load('rouge')
+
+original_model_results = rouge.compute(
+    predictions=original_model_summaries,
+    references=human_baseline_summaries[0:len(original_model_summaries)],
+    use_aggregator=True,
+    use_stemmer=True,
+)
+
+peft_model_results = rouge.compute(
+    predictions=peft_model_summaries,
+    references=human_baseline_summaries[0:len(peft_model_summaries)],
+    use_aggregator=True,
+    use_stemmer=True,
+)
+
+print('ORIGINAL MODEL:')
+print(original_model_results)
+print('PEFT MODEL:')
+print(peft_model_results)
+
+print("Absolute percentage improvement of PEFT MODEL over ORIGINAL MODEL")
+
+improvement = (np.array(list(peft_model_results.values())) - np.array(list(original_model_results.values())))
+for key, value in zip(peft_model_results.keys(), improvement):
+    print(f'{key}: {value*100:.2f}%')
+
+
+## YODA TUTORIAL REFERENCE
+# model_repo = "microsoft/Phi-3-mini-4k-instruct"
+# model = get_model(model_repo) 
+# print(f"Model:\n{model}")
+# print(f"{model.get_memory_footprint()/1e6}MB footprint")
+
+# # Quantised model can be used for inference, but can't be fine-tuned.
+# # So we need to setup low-rank adapters (LoRA)
+# # LoRA can be attached to each quantised layer (print(model) to see
+# # layers)
+
+# # LoRA are mostly regular layers which can be updated as usual, but 
+# # they manage to be significantly smaller than the quantised layers.
+# # This drastically reduces the number of total trainable params to
+# # 1% or less.
+# model = prepare_model_for_kbit_training(model)
+# config = LoraConfig(
+#     # Rank of adapter. Proportional to num params to train.
+#     r=8,
+#     # Multiplier, usually 2 * r.
+#     lora_alpha=16,
+#     bias="none",
+#     lora_dropout=0.05,
+#     task_type="CAUSAL_LM",
+#     # Newer models like Phi-3 may require manually setting target
+#     # modules
+#     target_modules=["o_proj", "qkv_proj", "gate_up_proj", "down_proj"]
+# )   
+# model = get_peft_model(model, config)
+# print(f"Peft Model:\n{model}")
+# print(f"{model.get_memory_footprint()/1e6}MB footprint")
+# train_p, tot_p = model.get_nb_trainable_parameters()
+# print(f'Trainable parameters:      {train_p/1e6:.2f}M')
+# print(f'Total parameters:          {tot_p/1e6:.2f}M')
+# print(f'% of trainable parameters: {100*train_p/tot_p:.2f}%')
+# print({p.dtype for p in model.parameters()})
+
+# # Trim dataset
+# dataset = load_dataset("dvgodoy/yoda_sentences", split="train")
+# dataset = dataset.rename_column("sentence", "prompt")
+# dataset = dataset.rename_column("translation_extra", "completion")
+# dataset = dataset.remove_columns(["translation"])
+# messages = [
+#     {"role": "user", "content": dataset[0]['prompt']},
+#     {"role": "assistant", "content": dataset[0]['completion']}
+# ]
+
+# # Format dataset into conversational format
+# def format_dataset(examples):
+#     # If prompts is a list (standard)
+#     if isinstance(examples["prompt"], list):
+#         output_texts = []
+#         for i in range(len(examples["prompt"])):
+#             converted_sample = [
+#                 {"role": "user", "content": examples["prompt"][i]},
+#                 {"role": "assistant", "content": examples["completion"][i]}
+#             ]
+#             output_texts.append(converted_sample)
+#         return {"messages": output_texts}
+#     else:
+#         converted_sample = [
+#             {"role": "user", "content": examples["prompt"]},
+#             {"role": "assistant", "content": examples["completion"]}
+#         ]
+#         return {"messages": converted_sample}
+# dataset = dataset.map(format_dataset).remove_columns(["prompt", "completion"])
+
+# # Load the tokeniser corresponding to our model.
+# tokenizer = get_tokenizer(model_repo)
+# print(tokenizer.apply_chat_template(messages, tokenize=False))
+
+# # Fine-tuning - follows exact same training procedure as training from scratch.
+# # https://huggingface.co/blog/dvgodoy/fine-tuning-llm-hugging-face#fine-tuning-with-sfttrainer
+# sft_config = SFTConfig(
+#     # Mem usage
+#     gradient_checkpointing=True, # Saves a lot of mem
+#     gradient_checkpointing_kwargs={'use_reentrant': False}, # Prevent exceptions in new PyTorch
+#     gradient_accumulation_steps=1, # Actual batch (for updating) is same as micro batch size
+#     per_device_train_batch_size=16, # Micro batch size to begin with
+#     auto_find_batch_size=True, # Halves batch size if it were to cause OOM, until it works
+
+#     # Dataset
+#     max_length=64,
+#     packing=True, # No padding is needed
+
+#     # Training params
+#     num_train_epochs=10,
+#     learning_rate=3e-5,
+#     # Doesn't help much if using LoRA (which we are)
+#     optim="paged_adamw_8bit",
+
+#     # Logging params
+#     logging_steps=10,
+#     logging_dir="./logs",
+#     output_dir="./phi3-mini-yoda-adapter",
+#     report_to="none"
+# )
+
+# trainer = SFTTrainer(
+#     model=model,
+#     processing_class=tokenizer,
+#     args=sft_config,
+#     train_dataset=dataset,
+# )
+
+# # Labels were added automatically, and are the same as the inputs
+# # Hence "self-supervised fine-tuning"
+# trainer.train()
+# trainer.save_model("local-phi3-mini-yoda-adapter")
+
+# # Prompt
+# prompt = gen_prompt(tokenizer, "The Force is strong with you!")
+# print(gen_completion(model, tokenizer, prompt))
